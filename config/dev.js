94e2bfcd14c3637cfcef994920d3849ef3ae8808
Eldor Togaymurotov <eldorcodes@gmail.com>
2021-01-27 18:11:27 -0500

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://russellexpressllc:AFBYXGGmaVXUDxCL@russellexpressllc.g1plt.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    NextmoApiKey: 'ac46ed44',
-    NextmoApiSecret: 'zhVHRAE7jajwwiJM',
-    GoogleMapApiKey: 'AIzaSyCazsdPAkeA3D7dMpMwENAx9hIyXa2dxIE',
-    AWSAccessID: 'AKIA45Z4RLTJ4ESKXF7J',
-    AWSSecretKey: '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',
-};
