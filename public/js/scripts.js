f0f059c74c6f58896c7bf192964ce26817d897ad
eldorcodes <russellexpressllc@gmail.com>
2021-01-29 15:28:35 -0500

    progress bar added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,12 +4,32 @@ jQuery(document).ready(function(){
   socket.on('connect',function(socket){
     console.log('Connected to Server');
   });
+  function uploadProgressHandler(event) {
+    $("#loaded_n_total").html("Uploaded " + event.loaded + " bytes of " + event.total);
+    var percent = (event.loaded / event.total) * 100;
+    var progress = Math.round(percent);
+    $("#uploadProgressBar").html(progress + " percent na ang progress");
+    $("#uploadProgressBar").css("width", progress + "%");
+    $("#status").html(progress + "% uploaded... please wait");
+}
+
+function loadHandler(event) {
+    $("#status").html(event.target.responseText);
+    $("#uploadProgressBar").css("width", "0%");
+}
+
+function errorHandler(event) {
+    $("#status").html("Upload Failed");
+}
+
+function abortHandler(event) {
+    $("#status").html("Upload Aborted");
+}
   $('#upload-input').on('change',function(){
     var uploadInput = $('#upload-input');
     if(uploadInput.val() != ''){
         var formData = new FormData();
         formData.append('image',uploadInput[0].files[0]);
-
         // make ajax request to send image to database
         $.ajax({
             url: '/uploadImage',
@@ -17,6 +37,18 @@ jQuery(document).ready(function(){
             data: formData,
             processData: false,
             contentType: false,
+            xhr: function () {
+              var xhr = new window.XMLHttpRequest();
+              xhr.upload.addEventListener("progress",
+                  uploadProgressHandler,
+                  false
+              );
+              xhr.addEventListener("load", loadHandler, false);
+              xhr.addEventListener("error", errorHandler, false);
+              xhr.addEventListener("abort", abortHandler, false);
+
+              return xhr;
+          },
             success: function(){
                 uploadInput.val('');
             }
